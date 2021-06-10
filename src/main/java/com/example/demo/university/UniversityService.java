3a764b7a0efeb861ea6af06618c06870900347e4
Darius Tinculescu <dariustinculescu@gmail.com>
2021-06-10 05:32:23 +0300

    updateStudent() && updateUniversity has been added! + Code format

--- src/main/java/com/example/demo/university/UniversityService.java
+++ src/main/java/com/example/demo/university/UniversityService.java
@@ -3,7 +3,9 @@ package com.example.demo.university;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -16,13 +18,13 @@ public class UniversityService {
         this.universityRepository = universityRepository;
     }
 
-    public List<University> getUniversities(){
+    public List<University> getUniversities() {
         return universityRepository.findAll();
     }
 
     public void addNewUniversity(University university) {
         Optional<University> universityOptional = universityRepository.findUniversityByName(university.getName());
-        if(universityOptional.isPresent()){
+        if (universityOptional.isPresent()) {
             throw new IllegalStateException("University already exist!");
         }
         universityRepository.save(university);
@@ -30,9 +32,29 @@ public class UniversityService {
 
     public void deleteUniversity(Long universityId) {
         boolean exists = universityRepository.existsById(universityId);
-        if(!exists){
+        if (!exists) {
             throw new IllegalStateException("University with id " + universityId + " does not exist!");
         }
         universityRepository.deleteById(universityId);
+    }
+
+    @Transactional
+    public void updateUniversity(Long universityId, String name, String country, String city) {
+        University university = universityRepository.findById(universityId).orElseThrow(() ->
+                new IllegalStateException("University with id " + universityId + " does not exist!"));
+        if (name != null && name.length() > 0 &&
+                !Objects.equals(university.getName(), name)) {
+            university.setName(name);
+        }
+        if (country != null && country.length() > 0 &&
+                !Objects.equals(university.getCountry(), country)) {
+            university.setCountry(country);
+        }
+        if (city != null && city.length() > 0 &&
+                !Objects.equals(university.getCity(), city)) {
+            university.setCity(city);
+        }
+
+
     }
 }
